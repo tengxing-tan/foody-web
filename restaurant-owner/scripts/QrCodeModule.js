b015224f18ac926fde506ea491885954ba4885a7
Tan Teng Xing <tantengxing@gmail.com>
2022-06-10 12:10:43 +0800

    fix qrcode

--- restaurant-owner/scripts/QrCodeModule.js
+++
@@ -1,8 +0,0 @@
-var qrcode = new QRCode(document.getElementById('qr-code'), {
-    text: "http://10.66.0.36:3000/webserver/foody/restaurant-owner/view-charts.php?id=",
-    width: 128,
-    height: 128,
-    colorDark: "#6C5A8A",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H
-});
