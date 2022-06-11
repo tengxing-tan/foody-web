1749d8ddc6c99ce5b75a29a181ad26739b326f5d
Tan Teng Xing <tantengxing@gmail.com>
2022-06-11 16:20:43 +0800

    update order list to summary

--- restaurant-owner/assets/reusable/navbar.php
+++ restaurant-owner/assets/reusable/navbar.php
@@ -3,9 +3,8 @@
      <ul>
          <li><a id="nav-3" class="nav-link" href="index.php">Order Status</a></li>
          <li><a id="nav-2" class="nav-link" href="menu-list.php">Menu List</a></li>
-         <li><a id="nav-4" class="nav-link" href="order-list.php">Order List</a></li>
+         <li><a id="nav-4" class="nav-link" href="order-summary.php">Order Summary</a></li>
          <li><a id="nav-5" class="nav-link" href="sales-report.php">Sales Report</a></li>
-         <li><a id="nav-6" class="nav-link" href="my-restaurants.php">My Restaurant</a></li>
      </ul>
      <!-- logout, link to outside of the folder restaurant-owner -->
      <a href="../../../" class="nav-link" style="text-decoration: underline;">
